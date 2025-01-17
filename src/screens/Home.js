f5484c4fe77620025aa27a67ee469e43743426ce
Shrinivas <shrinivasulpe27@gmail.com>
2025-01-17 23:24:27 +0530

    upto bcrypt jwt routing

---
+++ src/screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { Card } from '../components/Card'
+import { Carousal } from '../components/Carousal'
+
+function Home() {
+    return (
+        <div>
+            <div> <Navbar /> </div>
+            <div> <Carousal /></div>
+            <div className='m-3'>
+                <Card />
+
+
+            </div>
+            <div> <Footer /> </div>
+        </div>
+    )
+}
+
+export default Home
